0da352f2a19fbc7d4e039be854620b463d33f1a8
花开陌路 <312781278@qq.com>
2025-02-26 22:50:47 +0800

    Update email.ts
    更改临时邮箱上限

--- app/config/email.ts
+++ app/config/email.ts
@@ -1,5 +1,5 @@
 export const EMAIL_CONFIG = {
-  MAX_ACTIVE_EMAILS: 30, // Maximum number of active emails
+  MAX_ACTIVE_EMAILS: 999999999, // Maximum number of active emails
   POLL_INTERVAL: 10_000, // Polling interval in milliseconds
 } as const
 
